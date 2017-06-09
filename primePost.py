d1f421fb919b96b39a5688788d745ae270c8c61d
mcamocci <mcamocci@gmail.com>
2017-06-09 13:25:32 +0300

    after the post we are finally done fellas

--- primePost.py
+++ primePost.py
@@ -44,27 +44,31 @@ def dated_url_for(endpoint, **values):
 #you will be redirect if not logged in else you directed to poster aka home
 @app.route("/")
 def index():
+    tarehe=utilityDate()
+    data={"tarehe":tarehe}
     if "user_id" in session:
         return redirect(url_for('home'))
-    return render_template("index.html")
+    return render_template("index.html",**data)
 
 ########################################################################################
 ########################################################################################
 #this is for the poster info pannel after the login
 @app.route("/home")
 def home():
+    tarehe=utilityDate()
     if "user_id" in session:
         posters=posterInfoDao.PosterInfoDao().getAllPosterInfo()
-        context={'posterInfos':posters}
+        context={'posterInfos':posters,"tarehe":tarehe}
         return render_template("poster.html",**context)
     else:
-        return render_template("index.html")
+        return render_template("index.html",**data)
 
 ########################################################################################
 ########################################################################################
 #the things typed on form handled here
 @app.route("/login",methods=["POST"])
 def login():
+    tarehe=utilityDate()
     if request.method=="POST":
         username=request.form.get("username",None)
         password=request.form.get("password",None)
@@ -88,11 +92,13 @@ def login():
 #the logout handler function
 @app.route("/logout")
 def logout():
+    tarehe=utilityDate()
+    tarehe={"tarehe":tarehe}
     if "user_id" in session:
         session.pop("user_id",None)
-        return render_template("index.html")
+        return render_template("index.html",**tarehe)
     else:
-        return render_template("index.html")
+        return render_template("index.html",**tarehe)
 
 ########################################################################################
 ########################################################################################
@@ -104,12 +110,14 @@ def admin():
 ########################################################################################
 ########################################################################################
 #getting posts from the specified channel
-@app.route("/posts/<int:id>")
+@app.route("/posts")
 def posts(id=None):
+    tarehe=utilityDate()
+    print("hello there , its seems you are requesting for the posts ")
+    id=session['current_poster']
     if id != None:
-        session['current_poster']=id
         posts=postDao.PostDao().getAllPost(id)
-        context={'posts':posts}
+        context={'posts':posts,"tarehe":tarehe}
         return render_template("home.html",**context)
 
     return redirect(url_for('index'))
@@ -118,9 +126,17 @@ def posts(id=None):
 ########################################################################################
 
 #getting posts from the specified channel
-@app.route("/publish")
-def publish():
-    return render_template("publishPost.html")
+@app.route("/publish/<int:id>")
+def publish(id=None):
+    tarehe=utilityDate()
+    tarehe={"tarehe":tarehe}
+    if id !=None:
+        session['current_poster']=id
+        return render_template("publish.html",**tarehe)
+    else:
+    	return redirect(url_for('index'),**tarehe)
+
+
 
 ########################################################################################
 ########################################################################################
@@ -134,7 +150,7 @@ def addCategory():
 ########################################################################################
 ########################################################################################
 
-@app.route("/publish",methods=["POST"])
+@app.route("/publishPost",methods=["POST"])
 def publishPost():
     if request.method=="POST":
         fileList=request.files.getlist("RESOURCES[]")
@@ -147,10 +163,10 @@ def publishPost():
                 file.save(os.path.join(app.config["UPLOAD_FOLDER"],filename))
                 uploaded_file_path.append(filename)
 
-        user_id=session["user_id"]
         poster_id=session['current_poster']
 
         inserted_post_id=postDao.PostDao().insertPost(poster_id,postDescription)
+        print(inserted_post_id)
 
         if(len(uploaded_file_path)>0):
             for fileurl in uploaded_file_path:
@@ -170,8 +186,19 @@ def deletePost(id=0):
         return redirect(url_for('posts',id=session['current_poster']))
     return redirect(url_for('index'))
 
+@app.route("/timetime")
+def utilityDate():
+    import datetime
+    from datetime import date
+    import calendar
+    day=calendar.day_name[date.today().weekday()]
+    mydate = datetime.datetime.now()
+    now = datetime.datetime.now()
+    day+=", "+str(now.day)+" "+mydate.strftime("%B")+" "+str(now.year)
+    return day
+
+
 
 if __name__=="__main__":
     app.secret_key="haikarose"
     app.run(debug=True,host='0.0.0.0')
-
